6c7f01bcfa59b759dfbf2f7eca514335207659e4
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2025-11-24 20:37:27 -0300

    navbar que se oculta y todo en la misma página

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+
+//barra de navegación que se oculta, cortesía del siguiente tutorial:
+// https://webdesign.tutsplus.com/how-to-hidereveal-a-sticky-bottom-nav-bar-on-scroll-with-javascript--cms-107947t 
+const body = document.body;
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+  if (currentScroll > lastScroll && !body.classList.contains(scrollUp)) {
+    // down 
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains(scrollUp)
+  ) {
+    // up
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  }
+  lastScroll = currentScroll;
+});
+
+
 function noAd(bool){
     var ad = document.getElementById("ad");
     if (bool){
